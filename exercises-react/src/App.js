586315a6e2253d4fc0c0fe7312c8eac7d78c0c02
SheylaPDV <92029579+SheylaPDV@users.noreply.github.com>
2023-03-13 09:20:10 +0100

    Clock component

--- exercises-react/src/App.js
+++ exercises-react/src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ComponentA from "./Exercises/ComponentA";
+import Clock from "./Exercises/Clock";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ComponentA />
+        {/* <ComponentA /> */}
+        <Clock />
       </header>
     </div>
   );
